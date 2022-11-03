8b2ce31da14b5461886ee8b1bc18c2d4f7983e97
hugofilho83dev <hugotavares83.dev@gmail.com>
2022-11-03 11:34:42 -0300

    Ajusteste projeto de teste

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := soma(15, 15)
+	total := soma(15, 25)
 
 	if total != 30 {
 		t.Errorf("Resultado da soma e inválido: Resultado %d. Esperado: %d", total, 30)
